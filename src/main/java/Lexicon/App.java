26b6a3d51b511be9f9f1f3487fdef5ffeb532e82
Bassim Durzi <bassimdurzi@gmail.com>
2023-08-02 17:15:01 +0200

    Part1

--- src/main/java/Lexicon/App.java
+++ src/main/java/Lexicon/App.java
@@ -5,12 +5,10 @@ public class App {
     public static void main(String[] args) {
         fullName();
 
-
     }
 
     public static void fullName() {
         String[] fullNameArray = {"Bassim Durzi", "Shubha Dhang", "Negar Baharmand", "Juan Oliver"};
-
 
         for (int i = 0; i < fullNameArray.length; i++) {
             System.out.println(fullNameArray[i]);
--- src/main/java/Lexicon/NameRepository.java
+++ src/main/java/Lexicon/NameRepository.java
@@ -3,23 +3,40 @@ package Lexicon;
 import java.util.Arrays;
 
 public class NameRepository {
-    private static String [] names = new String[0];
-    public static int getSize(){
+
+    public static void main(String[] args) {
+        getSize();
+        setNames(names);
+        clear();
+        findAll();
+
+
+    }
+
+    private static String[] names = new String[0];
+    public static String[] newNames = {"Bassim Durzi", "Shubha Dhang", "Negar Baharmand", "Juan Oliver"};
+
+    public static int getSize() {
         int arraySize = names.length;
+        System.out.println("Number of elements " + arraySize);
         return arraySize;
     }
-    public static void setNames(String[] names) {
-        String[] newNames = Arrays.copyOf(names, names.length);
+
+    public static void setNames(String[] Names) {
+        names = Arrays.copyOf(newNames, newNames.length);
+        System.out.println(Arrays.toString(names));
     }
 
-    public static void clear(){
-        names = new String[0];
+    public static void clear() {
+        names = Arrays.copyOf(names, 0);
+        System.out.println(Arrays.toString(names));
     }
+
     public static String[] findAll() {
-        String[] newArray = Arrays.copyOf(names, names.length);
-        return newArray;
+        String[] returnNamesNewArray = Arrays.copyOf(newNames, newNames.length);
+        System.out.println(Arrays.toString(returnNamesNewArray));
+        return returnNamesNewArray;
     }
-
 
 
 }
