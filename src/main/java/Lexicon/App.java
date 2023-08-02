ae31d4e2ef636bbf6a4ae17b4041e46652d12abf
Bassim Durzi <bassimdurzi@gmail.com>
2023-08-02 11:27:30 +0200

    Part1

--- src/main/java/Lexicon/App.java
+++ src/main/java/Lexicon/App.java
@@ -2,9 +2,19 @@ package Lexicon;
 
 public class App {
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
+        fullName();
 
 
     }
 
+    public static void fullName() {
+        String[] fullNameArray = {"Bassim Durzi", "Shubha Dhang", "Negar Baharmand", "Juan Oliver"};
+
+
+        for (int i = 0; i < fullNameArray.length; i++) {
+            System.out.println(fullNameArray[i]);
+
+        }
+    }
 }
